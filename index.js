b846fd2368fe5e03a36089c267567c9ec2cc3118
ParvesPR <parves2585@gmail.com>
2022-04-22 13:49:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Mongodb
+
+const uri = "mongodb+srv://dbuser:FSVHHKsjImVSDSLB@cluster0.z9ugs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db('mealExpress').collection('user');
+
+        app.post('/', (req, res) => {
+
+        })
+
+    }
+
+    finally {
+
+    }
+
+}
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('CRUD running node server')
+});
+
+app.listen(port, () => {
+    console.log('running CRUD')
+});
